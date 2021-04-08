c00124cf3757dd769c259e30807b6e0e4ee6134e
Zach <zawood11@gmail.com>
2021-04-08 13:08:17 -0600

    commit tree 6998367cd65c9f0e41770cd6df24ed09387a18c5

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31, num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 40, num4 = 9;
+
+let mod = num3 % num4;
+
+let set = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+let max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
